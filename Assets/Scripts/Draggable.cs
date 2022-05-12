4d96ecc2c0e3cf4b66b049cfc93955f656874d3d
tamioTech <62820237+tamioTech@users.noreply.github.com>
2022-05-12 16:57:05 -0700

    scrapped, now a clickable glove

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour, IPointerDownHandler ,IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("I got clicked");
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("begin drag");
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("dragging");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("stop dragging");
+    }
+
+}
